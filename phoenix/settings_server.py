077b4ca6d8c9be3118a1a218ec63f5241464589a
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2024-05-08 22:29:28 +0330

    create settings_server

---
+++ phoenix/settings_server.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'django-insecure-e=r6(=1-$v62u+!!y@45c2s6w)=^20(r1eq*z_k2b2brp7x6#)'
+
+ 
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+##########################################################
+#  installed apps
+ 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# INSTALLED_APPS.append("core")
+
+
+##########################################################
+#  databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+##########################################################
+#  url
+SITE_URL="/"
+STATIC_URL = SITE_URL+'static/'
+MEDIA_URL = SITE_URL+'media/'
+QRCODE_URL = SITE_URL+'qrcode/'
+FILE_URL = SITE_URL+'file/'
+
+##########################################################
+#  root
+PUBLIC_ROOT="/"
+PRIVATE_ROOT="/"
+MEDIA_ROOT =os.path.join(PUBLIC_ROOT,"media")
+STATIC_ROOT =os.path.join(PUBLIC_ROOT,"static")
+QRCODE_ROOT =os.path.join(PUBLIC_ROOT,"qrcode")
+FILE_ROOT =os.path.join(PRIVATE_ROOT,"file")
